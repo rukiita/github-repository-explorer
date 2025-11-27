2ae871ba16f172307db45cff7d4a7861a3893de9
rukiita <saibatelu@gmail.com>
2025-11-27 15:51:03 +0900

    add implementation of route handler

---
+++ app/api/github/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const q = searchParams.get("q");
+  const sort = searchParams.get("sort") || "best-match";
+  const lang = searchParams.get("lang");
+  const page = searchParams.get("page") || "1";
+  const perPage = searchParams.get("per_page") || "10";
+
+  if (!q) {
+    return NextResponse.json({ item: [], total_count: 0 });
+  }
+
+  const githubQuery = `${q} ${lang ? `language:${lang}` : ""}`.trim();
+
+  try {
+    const res = await fetch(
+      `https://api.github.com/search/repositories?q=${encodeURIComponent(
+        githubQuery
+      )}&sort=${sort}&page=${page}&per_page=${perPage}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+          accept: "application/vnd.github+json",
+        },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`Github API Error:${res.status}`);
+    }
+
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch data" },
+      { status: 500 }
+    );
+  }
+}
--- lib/api/github.ts
+++ lib/api/github.ts
@@ -1,6 +1,3 @@
-
-
-
 export const fetchRepos = async ({ pageParam = 1, queryKey }: any) => {
   const [_, q, sort, lang] = queryKey; //destructuring of queryKey
   if (!q) return { items: [], total_count: 0 };
@@ -10,7 +7,7 @@ export const fetchRepos = async ({ pageParam = 1, queryKey }: any) => {
     lang: lang || "",
     page: pageParam.toString(),
   });
-  const res = await fetch(`api/github?${params}`);
+  const res = await fetch(`api/github?${params.toString()}`);
   if (!res.ok) throw new Error("Network response was not ok");
   return res.json();
 };
