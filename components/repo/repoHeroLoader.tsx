590e4a2a20139f70d77d00c924de2fbfb4616dcb
rukiita <saibatelu@gmail.com>
2025-12-04 00:28:56 +0900

    split the data-fetching and display logic from client Component into a ServerComponent

---
+++ components/repo/repoHeroLoader.tsx
@@ -0,0 +1,18 @@
+import { getGithubRepository } from "@/lib/repositories/github";
+import RepoHero from "./repoHero";
+
+interface RepoHeroLoaderProps {
+  owner: string;
+  repo: string;
+}
+
+export default async function RepoHeroLoader({
+  owner,
+  repo,
+}: RepoHeroLoaderProps) {
+  const repository = await getGithubRepository().fetchRepoDetail(owner, repo);
+
+  if (!repository) return <div>Repository not found.</div>;
+
+  return <RepoHero repository={repository} />;
+}
