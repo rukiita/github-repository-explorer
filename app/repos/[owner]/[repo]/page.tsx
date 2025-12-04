168c8a488c2e9dcf7646ef25d235cede8c2de547
rukiita <saibatelu@gmail.com>
2025-12-04 10:46:59 +0900

    complete spliting the data-fetching and display logic from client Component into a ServerComponent

--- app/repos/[owner]/[repo]/page.tsx
+++ app/repos/[owner]/[repo]/page.tsx
@@ -1,9 +1,8 @@
-"use client";
-import { use } from "react";
-import { useReadme, useRepository } from "@/hooks/useGithub";
-import RepoHeroLoader from "@/components/repo/RepoHeroLoader";
-import RepoActionsLoader from "@/components/repo/RepoHeroLoader";
+import { Suspense, use } from "react";
+import RepoActionsLoader from "@/components/repo/repoActionsLoader";
 import ReadmeViewerLoader from "@/components/repo/readmeViewerLoader";
+import { Skeleton } from "@/components/ui/skeleton";
+import RepoHeroLoader from "@/components/repo/repoHeroLoader";
 
 interface RepoDetailPageProps {
   params: Promise<{
@@ -18,13 +17,23 @@ export default function RepoDetailPage({ params }: RepoDetailPageProps) {
   return (
     <>
       <div className="mx-4">
-        <section className="my-4">
+        <Suspense fallback={<Skeleton className="h-40 w-full rounded-xl" />}>
           <RepoHeroLoader owner={owner} repo={repo} />
           <RepoActionsLoader owner={owner} repo={repo} />
-        </section>
-        <section>
+        </Suspense>
+
+        <Suspense
+          fallback={
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-3/4" />
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-5/6" />
+              <Skeleton className="h-64 w-full" />
+            </div>
+          }
+        >
           <ReadmeViewerLoader owner={owner} repo={repo} />
-        </section>
+        </Suspense>
       </div>
     </>
   );
