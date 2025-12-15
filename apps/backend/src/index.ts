cbaa93be808e19a3ce9759c83f59533990288dab
rukiita <saibatelu@gmail.com>
2025-12-15 14:16:21 +0900

    frontend server setup

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,24 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  "/*",
+  cors({
+    origin: "http://localhost:5173",
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  })
+);
 
-export default app
+const route = app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
+export type AppType = typeof route;
+
